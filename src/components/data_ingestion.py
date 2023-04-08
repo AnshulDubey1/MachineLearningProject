e97d2c26009ee209da9acc033457fcc11e7a5432
AnshulDubey1 <anshuldubey.2012@gmail.com>
2023-04-08 15:31:05 +0530

    Bug fixes and stuff

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass  
 class DataIngestionConfig:
@@ -32,3 +33,13 @@ class DataIngestion:
         
         except Exception as E:
            raise CustomException(E,sys)  
+    
+if __name__=="__main__":
+    obj =DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    data_transformation=DataTransformation()
+    train_arr,test_arr=data_transformation.initiate_data_transformation(train_data,test_data)
+    model_trainer=ModelTrainer()
+    model_trainer.initiate_model_trainer(train_arr,test_arr)
+
+
